e8b32122b78ef5bdae4161d38baba2742c88fe13
Srimathi <srimathi.j2654@gmail.com>
2024-06-26 20:49:49 +0530

    Functions added

---
+++ Functions.java
@@ -0,0 +1,14 @@
+package Java;
+import java.util.*;
+public class Functions {
+    public  static void printMyname(String name){
+        System.out.println("Her name is "+name);
+        return ;
+    }
+    public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+        System.out.println("Enter your name:");
+        String name = s.next();
+        printMyname(name);
+    }
+}
